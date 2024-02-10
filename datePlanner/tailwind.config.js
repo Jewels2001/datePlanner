6135d89c1a6b75a4f6cc31f192724f15f0c60045
Jewels2001 <juliewq25@gmail.com>
2024-02-10 18:36:29 -0500

    Add: initial frontend Vue framework and TailwindCSS

---
+++ datePlanner/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+	  "./index.html",
+	  "./src/**/*.{vue,js,jsx}",
+    './pages/**/*.{vue,html,js}',
+    './components/**/*.{vue,html,js}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
