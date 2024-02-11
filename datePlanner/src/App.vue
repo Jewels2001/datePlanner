21dde4a5a318f478e74c5955aeec7789cc12a3fb
Jewels2001 <juliewq25@gmail.com>
2024-02-10 20:31:06 -0500

    Add: navbar and removed template styling

--- datePlanner/src/App.vue
+++ datePlanner/src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue'
 import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
   <header>
-  <div class="text-green-700 text-xl">Hello, from TailwindCSS!</div>
+    <NavBar />
   </header>
 
   <main>
@@ -16,6 +17,8 @@ import TheWelcome from './components/TheWelcome.vue'
 <style scoped>
 header {
   line-height: 1.5;
+  padding: 0;
+  margin: 2;
 }
 
 .logo {
@@ -23,21 +26,4 @@ header {
   margin: 0 auto 2rem;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
