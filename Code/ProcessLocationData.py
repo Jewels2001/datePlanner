e40388ae688627d54a7260aa241c82a6786c71da
Matthew Badal-Badalian <badalbam@mcmaster.ca>
2024-02-11 15:03:57 -0500

    Updated

--- Code/ProcessLocationData.py
+++ Code/ProcessLocationData.py
@@ -1,18 +1,44 @@
 import pandas as pd
+import time
 
 #################################################################################################################
 ### Other Functions
 
-def ProcessLocationDF():
+def ProcessLocationDF(price=None):
     locations_DF = pd.read_csv('Data\locations.csv')
-    return locations_DF
+    if price is not None:
+        excluded_subcategories = list(set(locations_DF['Subcategory'].tolist()))
+        excluded_subcategories = list(filter(lambda x: x not in ['malls','dancing','parks','recreation','yoga studios','museums','theaters','cinemas'], excluded_subcategories))
+        filtered_locations_DF = locations_DF[~((locations_DF['Subcategory'].isin(excluded_subcategories)) & (locations_DF['price'].isna()))]
+        filtered_locations_DF = filtered_locations_DF.copy()
+        filtered_locations_DF['price'].fillna('Free/Unknown', inplace=True)
+        locations_processed_DF = filtered_locations_DF.copy()   
+        if price == 'Cheap':
+            price = '$'
+        elif price == 'Moderate':
+            price = '$$'
+        elif price == 'Pricey':
+            price = '$$$'
+        else:
+            price = '$$$$' 
+        locations_processed_DF = locations_processed_DF[locations_processed_DF['price'].isin([price, 'Free/Unknown'])].copy()
+
+       
+    locations_processed_DF.to_csv('Data\locations_processed.csv')    
+    return locations_processed_DF
 
 #################################################################################################################
 ### Variables
+
+price = 'Cheap'
 
 #################################################################################################################
 ### Main Code
 
 if __name__ == "__main__":
-    locations_DF = ProcessLocationDF()
-    print(locations_DF.head())
+    start_time = time.time()
+    locations_processed_DF = ProcessLocationDF(price)
+    print(locations_processed_DF.head())
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(f"Execution time: {execution_time} seconds")
